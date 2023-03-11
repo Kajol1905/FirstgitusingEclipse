d8fa05b6d73d681435de8636cddecee97927a715
Kajol1905 <kajo190599@gmail.com>
2023-03-11 14:12:35 +0530

    This is my first commit msg

--- src/firstGIT/Sample1.java
+++ src/firstGIT/Sample1.java
@@ -4,8 +4,7 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Sample Program for GIT using Eclipse");
-
+System.out.println("Sample Program for GIT using Eclipse");
 	}
 
 }
